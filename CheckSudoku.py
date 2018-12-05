c329aa02da788b8088652181dfa0424b878e731a
Garvey98 <864229420@qq.com>
2018-12-05 15:34:54 +0800

    Update gitignore

--- CheckSudoku.py
+++ CheckSudoku.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 
-import numpy as np
-
-# SudokuCase = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9],
-#                        [4, 5, 6, 7, 8, 9, 1, 2, 3],
-#                        [7, 8, 9, 1, 2, 3, 4, 5, 6],
-#                        [2, 1, 4, 3, 6, 5, 8, 9, 7],
-#                        [3, 6, 5, 8, 9, 7, 2, 1, 4],
-#                        [8, 9, 7, 2, 1, 4, 3, 6, 5],
-#                        [5, 3, 1, 6, 4, 2, 9, 7, 8],
-#                        [6, 4, 2, 9, 7, 8, 5, 3, 1],
-#                        [9, 7, 8, 5, 3, 1, 6, 4, 2]])
-
-
 def CheckSudokuCase(SudokuCase):
     s = set()
 
     for i in range(9):
         for j in range(9):
-            if SudokuCase[i][j] in s:
+            if SudokuCase[i][j] == 0:
+                SudokuCase[i][j] = 0
+            elif SudokuCase[i][j] in s:
                 return False
             else:
                 s.add(SudokuCase[i][j])
         s.clear()
 
         for k in range(9):
-            if SudokuCase[k][i] in s:
+            if SudokuCase[k][i] == 0:
+                SudokuCase[k][i] = 0
+            elif SudokuCase[k][i] in s:
                 return False
             else:
                 s.add(SudokuCase[k][i])
@@ -35,7 +26,9 @@ def CheckSudokuCase(SudokuCase):
         for j in range(0, 9, 3):
             for k in range(i, i + 3):
                 for p in range(j, j + 3):
-                    if SudokuCase[k][p] in s:
+                    if SudokuCase[k][p] == 0:
+                        SudokuCase[k][p] = 0
+                    elif SudokuCase[k][p] in s:
                         return False
                     else:
                         s.add(SudokuCase[k][p])
@@ -43,11 +36,3 @@ def CheckSudokuCase(SudokuCase):
 
     return True
 
-
-# def main():
-#     cc = CheckSudokuCase(SudokuCase)
-#     print(cc)
-
-
-# if __name__ == "__main__":
-#     main()
