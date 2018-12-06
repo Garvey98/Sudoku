87bf1fb230b4bcc2107462406a02ddd4fa6fac51
Garvey98 <864229420@qq.com>
2018-12-06 17:09:43 +0800

    最小可能优先深搜

--- Solve_Sudoku.py
+++ Solve_Sudoku.py
@@ -1,142 +1,77 @@
-#!/usr/bin/python3
-
-from CheckSudoku import CheckSudokuCase
 import numpy as np
 import time
 
-Sudoku1 = np.loadtxt('shudu.txt', dtype=int)
-Sudoku2 = np.loadtxt('shudu.txt', dtype=int)
+class SolveMySudoku():
+    def __init__(self):
+        MySudoku = np.loadtxt('shudu.txt', dtype=int)
+        self.SolveTheSudoku(MySudoku)
 
 
-def SolveSudoku(Shudu):
-    i, j = start_point(Shudu)
-    # print(i,j)
-    s = set()
-    all_num = set([0,1,2,3,4,5,6,7,8,9])
-    if i>=8 and j>=8 and Shudu[i][j]!=0:
-        return True
-    Count_Block_num(Shudu)
-    s = Block_Num_P(Shudu, i, j)
-    for value in all_num - s:
-        Shudu[i][j] = value
-        Valid = CheckSudokuCase(Shudu)
-        if Valid:
-            Shudu[i][j] = value 
-            if not SolveSudoku(Sudoku1):
-                Shudu[i][j] = 0 
-            else:
+    def Smallest_Possibility_First_DFS(self, Blank, Row_Num, Col_Num, Block_Num, MySudoku):
+        if len(Blank) == 0:
+            print(MySudoku)
+            return True
+
+        All_Num = set(range(1,10))
+        Smallest_Possible_Case = 9
+        for (r,c) in Blank:
+            Possibility = All_Num - ( Row_Num[r] | Col_Num[c] | Block_Num[r//3][c//3] )
+            # print(Possibility, r, c)
+            if len(Possibility) <= Smallest_Possible_Case:
+                Smallest_Possible_Case = len(Possibility)
+                i = r
+                j = c
+      
+        if Smallest_Possible_Case == 0:
+            return False
+
+        # print("smallest: ", Possibility, r, c)
+        Blank.remove((i, j))
+        Possibility = All_Num - ( Row_Num[i] | Col_Num[j] | Block_Num[i//3][j//3] )
+        
+        for num in Possibility:
+            Row_Num[i].add(num)
+            Col_Num[j].add(num)
+            Block_Num[i//3][j//3].add(num)
+            MySudoku[i][j] = num
+            if self.Smallest_Possibility_First_DFS(Blank, Row_Num, Col_Num, Block_Num, MySudoku):
                 return True
-        else:
-            Shudu[i][j] = 0
-    return False
-
-
-s_0 = set(); s_1 = set(); s_2 = set(); s_3 = set(); s_4 = set(); s_5 = set(); s_6 = set(); s_7 = set(); s_8 = set()
-def Count_Block_num(Shudu):
-    s_0.clear(); s_1.clear(); s_2.clear(); s_3.clear(); s_4.clear(); s_5.clear(); s_6.clear(); s_7.clear(); s_8.clear(); 
-    for i in range(0,3):
-        for j in range(0,3):
-            s_0.add(Shudu[i][j])
-    for i in range(0,3):
-        for j in range(3,6):
-            s_1.add(Shudu[i][j])
-    for i in range(0,3):
-        for j in range(6,9):
-            s_2.add(Shudu[i][j])
-    for i in range(3,6):
-        for j in range(0,3):
-            s_3.add(Shudu[i][j])
-    for i in range(3,6):
-        for j in range(3,6):
-            s_4.add(Shudu[i][j])
-    for i in range(3,6):
-        for j in range(6,9):
-            s_5.add(Shudu[i][j])
-    for i in range(6,9):
-        for j in range(0,3):
-            s_6.add(Shudu[i][j])
-    for i in range(6,9):
-        for j in range(3,6):
-            s_7.add(Shudu[i][j])
-    for i in range(6,9):
-        for j in range(6,9):
-            s_8.add(Shudu[i][j])
+            else:
+                MySudoku[i][j] = 0
+                Row_Num[i].remove(num)
+                Col_Num[j].remove(num)
+                Block_Num[i//3][j//3].remove(num)
     
-
-
-def Block_Num_P(Shudu, r, c):
-    if r<3 and c<3:
-        return s_0
-    if r<3 and 3<=c<6:
-        return s_1
-    if r<3 and c>=6:
-        return s_2
-    if 3<=r<6 and c<3:
-        return s_3
-    if 3<=r<6 and 3<=c<6:
-        return s_4
-    if 3<=r<6 and c>=6:
-        return s_5
-    if r>=6 and c<3:
-        return s_6
-    if r>=6 and 3<=c<6:
-        return s_7
-    if r>=6 and c>=6:
-        return s_8
-
-
-def Num_Possible(Shudu):
-    Count_Block_num(Shudu)
-    num_p = np.zeros([9,9], dtype=int)
-    all_num = set([0,1,2,3,4,5,6,7,8,9])
-    s = set()
-    k = set()
-    num_0 = 0 #原来0的个数
-    num_00 = 0 #消除的0的个数
-    for i in range(9):
-        for j in range(9):
-            if Shudu[i][j] == 0:
-                num_0 += 1
-                for m in range(9):
-                    s.add(Shudu[i][m])
-                for n in range(9):
-                    s.add(Shudu[n][j])
-                k = Block_Num_P(Shudu, i, j)
-                # print(s | k)
-                num_p[i][j] = len(all_num-s)
-                if num_p[i][j] == 1:
-                    x = [y for y in all_num-s]
-                    # print(x)
-                    Shudu[i][j] = x[0]
-                    num_p[i][j] = 0
-                    num_00 += 1
-            s.clear()
-            k.clear()
-    # print(num_00)
-    # print(num_0)
-    # print(Shudu)
-    if num_0 > num_00 and num_00:
-        Num_Possible(Shudu)
-    else:
-        print("[TIME] hh ", time.time() - start_time)
-        SolveSudoku(Shudu)
+        Blank.add((i, j))
+        return False
     
     
-                
+    def SolveTheSudoku(self, MySudoku):
 
+        Col_Num = []
+        Row_Num = []
+        Block_Num = [[0]*3 for i in range(3)]
 
-def start_point(Shudu):
-    for i in range(9):
-        for j in range(9):
-            if not Shudu[i][j]:
-                return i, j
-    return i, j
+        for i in range(9):
+            Col_Num.append(set())
+            Row_Num.append(set())
+  
+        for i in range(3):
+            for j in range(3):
+                Block_Num[i][j] = set()
+  
+        Blank = set()
 
-
-if __name__ == '__main__':
-    
-    start_time = time.time()
-    # ans = SolveSudoku(Sudoku1)
-    Num_Possible(Sudoku1)
-    print("[TIME]", time.time() - start_time)
-
+        for i in range(9):
+            for j in range(9):
+                if MySudoku[i][j] != 0:
+                    if MySudoku[i][j] not in Row_Num[i]:
+                        Row_Num[i].add(MySudoku[i][j])
+                    if MySudoku[i][j] not in Col_Num[j]:  
+                        Col_Num[j].add(MySudoku[i][j])
+                    if MySudoku[i][j] not in Block_Num[i//3][j//3]:   
+                        Block_Num[i//3][j//3].add(MySudoku[i][j])
+                else:
+                    Blank.add((i,j))
+  
+        self.Smallest_Possibility_First_DFS(Blank, Row_Num, Col_Num, Block_Num, MySudoku)
