f0cc771cb8eed46bab9f2f6990abc0c6028fa1f1
Garvey98 <864229420@qq.com>
2018-12-09 14:38:10 +0800

    减少参数传递

--- solve_sudoku.py
+++ solve_sudoku.py
@@ -4,6 +4,8 @@ import numpy as np
 
 class SolveMySudoku():
     """ This is the class that solves the sudoku"""
+    mysudoku = np.zeros((9, 9), dtype=int)
+    print(mysudoku)
     def __init__(self, Soduku_Path):
         """ Load the sudoku from the file and call the solution function to output the result"""
         allmysudoku = np.loadtxt(Soduku_Path, dtype=int)
@@ -11,16 +13,17 @@ class SolveMySudoku():
         sudoku_now = np.split(allmysudoku, sudoku_count)
         with open('Sudoku_Problem_Answer.txt', 'a+') as sudoku_answer:
             sudoku_answer.truncate(0)
-            for mysudoku in sudoku_now:
-                self.solve_the_sudoku(mysudoku)
-                np.savetxt(sudoku_answer, mysudoku, fmt="%d")
+            for mysudoku1 in sudoku_now:
+                self.mysudoku = mysudoku1
+                self.solve_the_sudoku()
+                np.savetxt(sudoku_answer, self.mysudoku, fmt="%d")
                 sudoku_answer.write('\n')
 
-    def smallest_possibility_first_dfs(self, blank, row_num, col_num, block_num, mysudoku):
+    def smallest_possibility_first_dfs(self, blank, row_num, col_num, block_num):
         """ Select the location with the fewest possible values for depth-first search"""
         blank_length = len(blank)
         if blank_length == 0:
-            print(mysudoku)
+            print(self.mysudoku)
             return True
 
         all_num = set(range(1, 10))
@@ -46,10 +49,10 @@ class SolveMySudoku():
             row_num[i].add(num)
             col_num[j].add(num)
             block_num[i//3][j//3].add(num)
-            mysudoku[i][j] = num
-            if self.smallest_possibility_first_dfs(blank, row_num, col_num, block_num, mysudoku):
+            self.mysudoku[i][j] = num
+            if self.smallest_possibility_first_dfs(blank, row_num, col_num, block_num):
                 return True
-            mysudoku[i][j] = 0
+            self.mysudoku[i][j] = 0
             row_num[i].remove(num)
             col_num[j].remove(num)
             block_num[i//3][j//3].remove(num)
@@ -57,7 +60,8 @@ class SolveMySudoku():
         blank.add((i, j))
         return False
 
-    def solve_the_sudoku(self, mysudoku):
+
+    def solve_the_sudoku(self):
         """ Analyze rows, columns, and blocks of sudoku"""
 
         col_num = []
@@ -76,15 +80,15 @@ class SolveMySudoku():
 
         for i in range(9):
             for j in range(9):
-                if mysudoku[i][j] != 0:
-                    if mysudoku[i][j] not in row_num[i]:
-                        row_num[i].add(mysudoku[i][j])
-                    if mysudoku[i][j] not in col_num[j]:
-                        col_num[j].add(mysudoku[i][j])
-                    if mysudoku[i][j] not in block_num[i//3][j//3]:
-                        block_num[i//3][j//3].add(mysudoku[i][j])
+                if self.mysudoku[i][j] != 0:
+                    if self.mysudoku[i][j] not in row_num[i]:
+                        row_num[i].add(self.mysudoku[i][j])
+                    if self.mysudoku[i][j] not in col_num[j]:
+                        col_num[j].add(self.mysudoku[i][j])
+                    if self.mysudoku[i][j] not in block_num[i//3][j//3]:
+                        block_num[i//3][j//3].add(self.mysudoku[i][j])
                 else:
                     blank.add((i, j))
 
         self.smallest_possibility_first_dfs(
-            blank, row_num, col_num, block_num, mysudoku)
+            blank, row_num, col_num, block_num)
