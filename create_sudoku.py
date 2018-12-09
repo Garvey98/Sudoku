d12aa28f2fa444275c426bcd1ab2d27929344051
Garvey98 <864229420@qq.com>
2018-12-09 14:00:44 +0800

    整理代码

---
+++ create_sudoku.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+""" This is the module that generates the sudoku endings"""
+import random
+import numpy as np
+
+
+class CreateMySudoku():
+    """ This is the class that generates the sudoku endings"""
+    root_shudu = np.array([[1, 9, 6, 4, 3, 8, 7, 5, 2],
+                           [3, 8, 4, 5, 7, 2, 1, 6, 9],
+                           [7, 2, 5, 6, 1, 9, 3, 4, 8],
+                           [5, 7, 2, 1, 6, 3, 9, 8, 4],
+                           [6, 3, 1, 8, 9, 4, 5, 2, 7],
+                           [9, 4, 8, 2, 5, 7, 6, 1, 3],
+                           [2, 5, 7, 9, 4, 1, 8, 3, 6],
+                           [4, 1, 9, 3, 8, 6, 2, 7, 5],
+                           [8, 6, 3, 7, 2, 5, 4, 9, 1]])
+
+    def __init__(self, count):
+        """ Initializes the generated file and calls the generation function """
+        with open('sudoku.txt', 'a+') as sudoku_file:
+            sudoku_file.truncate(0)
+            for _ in range(count):
+                # random_num = random.randint(0, 9)
+                # root_shudu = Seed_Sudoku(random_num)
+                self.generate_sudoku()
+                np.savetxt(sudoku_file, self.root_shudu, fmt="%d")
+                sudoku_file.write('\n')
+
+    def data_exchange(self):
+        """ Data transformation"""
+        numofexchange = 1
+        while numofexchange < 20:
+            random_num = random.randint(2, 9)
+            if random_num == 9:
+                for i in range(9):
+                    for j in range(9):
+                        if self.root_shudu[i][j] == random_num:
+                            self.root_shudu[i][j] = 2
+                        elif self.root_shudu[i][j] == 2:
+                            self.root_shudu[i][j] = 9
+            else:
+                for i in range(9):
+                    for j in range(9):
+                        if self.root_shudu[i][j] == random_num:
+                            self.root_shudu[i][j] = random_num+1
+                        elif self.root_shudu[i][j] == random_num+1:
+                            self.root_shudu[i][j] = random_num
+            numofexchange += 1
+        return self.root_shudu
+
+    def rowcolumn_exchange(self):
+        """ Row and column transformation"""
+        numofexchange = 1
+        while numofexchange < 20:
+            rownum = random.randint(1, 8)
+            if (rownum % 3) == 2:
+                self.root_shudu[[rownum, rownum-1], :] = self.root_shudu[[rownum-1, rownum], :]
+            else:
+                self.root_shudu[[rownum, rownum+1], :] = self.root_shudu[[rownum+1, rownum], :]
+            self.root_shudu = self.root_shudu.T
+            numofexchange += 1
+        return self.root_shudu
+
+    def generate_sudoku(self):
+        """ Generate a certain number of sudoku endings"""
+        self.data_exchange()
+        self.rowcolumn_exchange()
---
+++ solve_sudoku.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+""" This is the module that solves the sudoku"""
+import numpy as np
+
+class SolveMySudoku():
+    """ This is the class that solves the sudoku"""
+    def __init__(self, Soduku_Path):
+        """ Load the sudoku from the file and call the solution function to output the result"""
+        allmysudoku = np.loadtxt(Soduku_Path, dtype=int)
+        sudoku_count = int(len(allmysudoku)/9)
+        sudoku_now = np.split(allmysudoku, sudoku_count)
+        with open('Sudoku_Problem_Answer.txt', 'a+') as sudoku_answer:
+            sudoku_answer.truncate(0)
+            for mysudoku in sudoku_now:
+                self.solve_the_sudoku(mysudoku)
+                np.savetxt(sudoku_answer, mysudoku, fmt="%d")
+                sudoku_answer.write('\n')
+
+    def smallest_possibility_first_dfs(self, blank, row_num, col_num, block_num, mysudoku):
+        """ Select the location with the fewest possible values for depth-first search"""
+        blank_length = len(blank)
+        if blank_length == 0:
+            print(mysudoku)
+            return True
+
+        all_num = set(range(1, 10))
+        smallest_possible_case = 9
+        for (row, col) in blank:
+            possibility = all_num - \
+                (row_num[row] | col_num[col] | block_num[row//3][col//3])
+            # print(possibility, row, col)
+            if len(possibility) <= smallest_possible_case:
+                smallest_possible_case = len(possibility)
+                i = row
+                j = col
+
+        if smallest_possible_case == 0:
+            return False
+
+        # print("smallest: ", possibility, row, col)
+        blank.remove((i, j))
+        possibility = all_num - \
+            (row_num[i] | col_num[j] | block_num[i//3][j//3])
+
+        for num in possibility:
+            row_num[i].add(num)
+            col_num[j].add(num)
+            block_num[i//3][j//3].add(num)
+            mysudoku[i][j] = num
+            if self.smallest_possibility_first_dfs(blank, row_num, col_num, block_num, mysudoku):
+                return True
+            mysudoku[i][j] = 0
+            row_num[i].remove(num)
+            col_num[j].remove(num)
+            block_num[i//3][j//3].remove(num)
+
+        blank.add((i, j))
+        return False
+
+    def solve_the_sudoku(self, mysudoku):
+        """ Analyze rows, columns, and blocks of sudoku"""
+
+        col_num = []
+        row_num = []
+        block_num = [[0]*3 for i in range(3)]
+
+        for i in range(9):
+            col_num.append(set())
+            row_num.append(set())
+
+        for i in range(3):
+            for j in range(3):
+                block_num[i][j] = set()
+
+        blank = set()
+
+        for i in range(9):
+            for j in range(9):
+                if mysudoku[i][j] != 0:
+                    if mysudoku[i][j] not in row_num[i]:
+                        row_num[i].add(mysudoku[i][j])
+                    if mysudoku[i][j] not in col_num[j]:
+                        col_num[j].add(mysudoku[i][j])
+                    if mysudoku[i][j] not in block_num[i//3][j//3]:
+                        block_num[i//3][j//3].add(mysudoku[i][j])
+                else:
+                    blank.add((i, j))
+
+        self.smallest_possibility_first_dfs(
+            blank, row_num, col_num, block_num, mysudoku)
